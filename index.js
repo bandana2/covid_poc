6eb3db74b8bb2af8e4d4329a653c5076a4a6bfcb
bandana2 <nagesh.bandaru@novartis.com>
2020-11-09 21:21:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("build"));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(8080, () => console.log("server running"));
