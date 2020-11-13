509ce1529540b13daec5e75dd7b23c3e93626050
bandana2 <nagesh.bandaru@novartis.com>
2020-11-14 01:44:45 +0530

    first commit

--- build/static/js/xlsx.1fa32005.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcovid_frontend=this.webpackJsonpcovid_frontend||[]).push([[2],{478:function(n,o){},568:function(n,o){},569:function(n,o){}}]);
-//# sourceMappingURL=xlsx.1fa32005.chunk.js.map
---
+++ build/static/js/xlsx.9e0cd728.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovid_frontend=this.webpackJsonpcovid_frontend||[]).push([[2],{503:function(n,o){},593:function(n,o){},594:function(n,o){}}]);
+//# sourceMappingURL=xlsx.9e0cd728.chunk.js.map
