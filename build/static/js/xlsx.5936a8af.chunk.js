bb3aa66dbefb216a72eaf3466b68df145781a775
bandana2 <nagesh.bandaru@novartis.com>
2020-11-25 19:47:02 +0530

    added new logics

---
+++ build/static/js/xlsx.5936a8af.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcovid_frontend=this.webpackJsonpcovid_frontend||[]).push([[2],{578:function(n,o){},668:function(n,o){},669:function(n,o){}}]);
+//# sourceMappingURL=xlsx.5936a8af.chunk.js.map
